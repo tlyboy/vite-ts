9f9711819476d80d37412c8d859cfefcbac41a78
Guany <1084072690@qq.com>
2023-05-26 09:50:09 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
+  theme: {
+    colors: {
+      brand: {
+        primary: '#3498db',
+      },
+    },
+  },
   presets: [
     presetAttributify(),
     presetUno(),
