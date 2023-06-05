61d72698c7bc247eeae4cf29153ccc6a3c518659
Guany <1084072690@qq.com>
2023-06-05 19:49:51 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,3 @@
 import '@unocss/reset/tailwind.css'
 import './assets/styles/main.css'
 import 'virtual:uno.css'
-
-const canvas = document.querySelector('canvas')!
-
-canvas.width = innerWidth
-canvas.height = innerHeight
-
-const ctx = canvas.getContext('2d')!
-
-ctx.translate(innerWidth / 2, innerHeight / 2)
-
-const gradient = ctx.createRadialGradient(0, 0, 0, 0, 0, 10)
-
-gradient.addColorStop(0, '#fff')
-gradient.addColorStop(1, '#000')
-
-ctx.fillStyle = gradient
-ctx.arc(0, 0, 10, 0, Math.PI * 2)
-ctx.fill()
