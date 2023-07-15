957008abf7cccfc71f1e8e02bc501f845e507622
Guany <1084072690@qq.com>
2023-07-15 14:30:43 +0800

    feat: add unplugin-auto-import and axios

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      imports: [{
+        axios: [
+          ['default', 'axios'],
+        ],
+      }],
+      dirs: ['./src/utils'],
+    }),
     UnoCSS(),
   ],
   resolve: {
